29def8a075f837acee78f718b25456d4e9760d0a
magg <magg.4@hotmail.com>
2018-05-18 10:22:15 -0500

    Se la clase actividadDao

---
+++ src/main/java/co/ufps/edu/dao/ActividadDao.java
@@ -0,0 +1,25 @@
+package co.ufps.edu.dao;
+
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import co.ufps.edu.bd.SpringDbMgr;
+
+public class ActividadDao {
+
+	SpringDbMgr springDbMgr = new SpringDbMgr();
+	
+
+	  /*
+	   *  Método que obtiene la cantidad de actividades registradas
+	   */
+	  public int getCntActividades() {
+		  	int cant = 0;
+		    // Consulta para realizar en base de datos
+		    SqlRowSet sqlRowSet = springDbMgr.executeQuery(" SELECT COUNT(*) cantidad FROM ACTIVIDAD "); 
+		    
+		    if (sqlRowSet.next()) {
+		    	cant = sqlRowSet.getInt("cantidad");
+		    }
+		    return cant;
+	  }
+}
